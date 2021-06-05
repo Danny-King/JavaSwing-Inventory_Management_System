51c110756b4aa951e1f64f89e9da040b0e34c41a
Danny <Danny@AVITA>
2021-06-05 17:14:12 +0800

    This is Object-Oriented Programming Group Project. Assignment 1 Group 9 Inventory Management System GUI.

--- src/inventory_management_system/Inventory_Management_System.java
+++ src/inventory_management_system/Inventory_Management_System.java
@@ -9,21 +9,14 @@
 package inventory_management_system;
 
 //Import statement
-import java.awt.Font;
-import java.awt.Image;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.*;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JLabel;
 
 /**
  *
--- src/inventory_management_system/Product.java
+++ src/inventory_management_system/Product.java
@@ -9,14 +9,8 @@
 package inventory_management_system;
 
 //Import statement
-import java.awt.Color;
-import java.awt.Font;
-import java.awt.Image;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.*;
+import java.awt.event.*;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -25,15 +19,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.JTextField;
+import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
 /**
